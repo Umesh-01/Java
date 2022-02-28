0dcbb525221a1aede394af470580ef326b2235bc
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-02-28 14:44:44 +0530

    Create KotMBank.java

---
+++ Assignments/Abstract Classes/KotMBank.java
@@ -0,0 +1,13 @@
+package extra;
+
+public class KotMBank extends GeneralBank {
+		
+	public double getSavingInterestRate() {
+		return 6.0;
+	}
+
+	public double getFixedInterestRate() {
+		return 9.0;
+	}
+
+}
