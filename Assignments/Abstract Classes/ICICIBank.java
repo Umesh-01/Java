c02f814af0c57e07a9ce996506c8091180b2f1ec
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-02-28 14:38:36 +0530

    Create ICICIBank.java

---
+++ Assignments/Abstract Classes/ICICIBank.java
@@ -0,0 +1,15 @@
+package extra;
+
+public class ICICIBank extends GeneralBank {
+
+	public double getSavingInterestRate() {
+		return 4.0;
+	}
+
+	public double getFixedInterestRate() {
+		return 8.5;
+	}
+
+}
+	
+
