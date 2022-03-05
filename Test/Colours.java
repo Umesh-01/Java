2359997755f754f5dc9d750a09c2c91a48f204b5
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-03-05 22:33:33 +0530

    Create Colours.java

---
+++ Test/Colours.java
@@ -0,0 +1,34 @@
+package extra;
+
+public class Colours {
+
+	public static void main(String[] args) {
+		char colorCode = 'Y';
+		
+		switch (colorCode) {
+		case 'R':
+			System.out.println("R->Red");
+			break;
+		case 'G':
+			System.out.println("G->Green");
+			break;
+		case 'B':
+			System.out.println("B->Blue");
+			break;
+		case 'O':
+			System.out.println("O->Orange");
+			break;
+		case 'Y':
+			System.out.println("Y->Yellow");
+			break;
+		case 'W':
+			System.out.println("W->White");
+			break;
+		default:
+			System.out.println("Invalid Code");
+		}
+
+	}
+
+	
+}
