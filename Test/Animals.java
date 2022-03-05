b4ad4235a25fb6f33acf4c9a4602a1b4ce8500c2
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-03-05 22:32:52 +0530

    Create Animals.java

---
+++ Test/Animals.java
@@ -0,0 +1,77 @@
+package extra;
+
+class Aniimal{
+
+  public void eat()  {
+
+    System.out.println("This is eat method");
+
+  }
+
+  public void sleep()  {
+
+    System.out.println("This is sleep method");
+
+  }
+
+
+}
+
+
+
+class Bird extends Aniimal{
+
+
+  public void eat() {
+
+    super.eat();
+
+    System.out.println("This is overriden eat method");
+
+  }
+
+
+
+  public void sleep() {
+
+    super.sleep();
+
+    System.out.println("This is overriden sleep method");
+
+  }
+
+
+
+  public void fly()
+
+  {
+
+    System.out.println("This is fly method");
+
+  }
+
+}
+
+
+
+public class Animals{
+
+  public static void main(String[] args) {
+
+    Aniimal a = new Aniimal();
+
+    a.eat();
+
+    a.sleep();
+
+    Bird b = new Bird();
+
+    b.eat();
+
+    b.sleep();
+
+    b.fly();
+
+  }
+
+}
