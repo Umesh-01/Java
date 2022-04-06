92b77f85dd52a4928bbd2eb879ab27b04132a496
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-04-06 11:56:34 +0530

    Create CharactersInOrder.java

---
+++ Assignments/CharactersInOrder.java
@@ -0,0 +1,33 @@
+package extra;
+
+
+
+public class CharactersInOrder {
+
+
+
+public static void main(String[] args) {
+
+char x = 's';
+
+char y = 'e';
+
+
+
+if (x < y) {
+
+System.out.println(x + ", " + y);
+
+} else {
+
+System.out.println(y + ", " + x);
+
+}
+
+
+
+}
+
+
+
+}
