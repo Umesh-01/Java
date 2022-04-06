9894f6ff617f8af99f833f11726a8191fd33115f
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-04-06 11:52:53 +0530

    Create CalculatePower.java

---
+++ Assignments/CalculatePower.java
@@ -0,0 +1,41 @@
+package extra;
+
+
+
+
+
+class Calculator {
+
+public static int powerInt(int num1, int num2) {
+
+return (int) Math.pow(num1, num2);
+
+}
+
+
+
+public static double powerDouble(double num1, int num2) {
+
+return Math.pow(num1, num2);
+
+}
+
+}
+
+
+
+public class CalculatePower {
+
+
+
+public static void main(String[] args) {
+
+System.out.println(Calculator.powerInt(5, 2));
+
+System.out.println(Calculator.powerDouble(2.5, 3));
+
+}
+
+
+
+}
