be2dbe66376e1491293d7e07df93803cf6d2a909
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-04-06 11:43:55 +0530

    Create ArrayProgram.java

---
+++ Assignments/ArrayProgram.java
@@ -0,0 +1,65 @@
+package extra;
+
+import java.util.InputMismatchException;
+
+import java.util.Scanner;
+
+
+public class ArrayProgram {
+
+public static void main(String[] args) {
+
+Scanner sc = new Scanner(System.in);
+
+
+System.out.println("Enter the number of elements you want in the array:");
+
+int n = sc.nextInt();
+
+
+
+int[] arr = new int[n];
+
+
+
+System.out.println("Enter the elements in the array: ");
+
+try {
+
+for (int i = 0; i < n; i++)
+
+arr[i] = sc.nextInt();
+
+
+
+System.out.println("Enter the index of the array element you want to access:");
+
+
+
+int index = sc.nextInt();
+
+System.out.println("The array element at index " + index + " = " + arr[index]);
+
+System.out.println("The array element successfully accessed.");
+
+} catch (ArrayIndexOutOfBoundsException e) {
+
+System.out.println("java.lang.ArrayIndexOutOfBoundsException");
+
+} catch (InputMismatchException e) {
+
+System.out.println("java.util.InputMismatchException");
+
+}
+
+
+
+sc.close();
+
+
+
+}
+
+
+
+}
