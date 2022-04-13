3745b129df5c6f643589fd9b054d011ca9e586d4
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-04-13 15:07:50 +0530

    Update CountryCapital.java

--- Assignments/CountryCapital.java
+++ Assignments/CountryCapital.java
@@ -1,138 +1,125 @@
 package extra;
 
-
-
 import java.util.ArrayList;
-
 import java.util.HashMap;
-
 import java.util.Iterator;
-
 import java.util.Map;
-
 import java.util.Map.Entry;
-
 import java.util.Set;
-
-
 
 class CountryMap {
 
-
-
 private HashMap<String, String> M1;
 
+  public CountryMap() {
 
+  M1 = new HashMap<String, String>();
 
-public CountryMap() {
+  }
 
-M1 = new HashMap<String, String>();
 
-}
 
+  public HashMap<String, String> saveCountryCapital(String CountryName, String capital) {
 
+  M1.put(CountryName, capital);
 
-public HashMap<String, String> saveCountryCapital(String CountryName, String capital) {
+  return M1;
 
-M1.put(CountryName, capital);
+  }
 
-return M1;
 
-}
 
+  public String getCapital(String CountryName) {
 
+  return M1.get(CountryName);
 
-public String getCapital(String CountryName) {
+  }
 
-return M1.get(CountryName);
 
-}
 
+  public String getCountry(String capitalName) {
 
+  Set<Entry<String, String>> set = M1.entrySet();
 
-public String getCountry(String capitalName) {
+  Iterator<Entry<String, String>> it = set.iterator();
 
-Set<Entry<String, String>> set = M1.entrySet();
 
-Iterator<Entry<String, String>> it = set.iterator();
 
+  while (it.hasNext()) {
 
+  Map.Entry<String, String> me = it.next();
 
-while (it.hasNext()) {
 
-Map.Entry<String, String> me = it.next();
 
+  if (me.getValue().equals(capitalName))
 
+  return me.getKey();
 
-if (me.getValue().equals(capitalName))
+  }
 
-return me.getKey();
 
-}
 
+  return null;
 
+  }
 
-return null;
 
-}
 
+  public HashMap<String, String> swapKyeValue() {
 
+  HashMap<String, String> M2 = new HashMap<String, String>();
 
-public HashMap<String, String> swapKyeValue() {
 
-HashMap<String, String> M2 = new HashMap<String, String>();
 
+  Set<Entry<String, String>> set = M1.entrySet();
 
+  Iterator<Entry<String, String>> it = set.iterator();
 
-Set<Entry<String, String>> set = M1.entrySet();
 
-Iterator<Entry<String, String>> it = set.iterator();
 
+  while (it.hasNext()) {
 
+  Map.Entry<String, String> me = it.next();
 
-while (it.hasNext()) {
+  M2.put(me.getValue(), me.getKey());
 
-Map.Entry<String, String> me = it.next();
+  }
 
-M2.put(me.getValue(), me.getKey());
 
-}
 
+  return M2;
 
+  }
 
-return M2;
 
-}
 
+  public ArrayList<String> toArrayList() {
 
+  ArrayList<String> list = new ArrayList<>();
 
-public ArrayList<String> toArrayList() {
 
-ArrayList<String> list = new ArrayList<>();
 
+  Set<Entry<String, String>> set = M1.entrySet();
 
+  Iterator<Entry<String, String>> it = set.iterator();
 
-Set<Entry<String, String>> set = M1.entrySet();
 
-Iterator<Entry<String, String>> it = set.iterator();
 
+  while (it.hasNext()) {
 
+  Map.Entry<String, String> me = it.next();
 
-while (it.hasNext()) {
+  list.add(me.getKey());
 
-Map.Entry<String, String> me = it.next();
+  }
 
-list.add(me.getKey());
 
-}
 
+  return list;
 
+  }
 
-return list;
-
-}
-
-}
+  }
 
 
 
@@ -140,36 +127,31 @@ public class CountryCapital {
 
 
 
-public static void main(String[] args) {
+  public static void main(String[] args) {
 
-CountryMap countryMap = new CountryMap();
-
-
-
-countryMap.saveCountryCapital("India", "Delhi");
-
-countryMap.saveCountryCapital("Japan", "Tokyo");
-
-countryMap.saveCountryCapital("USA", "Washington, D.C.");
+  CountryMap countryMap = new CountryMap();
 
 
 
-System.out.println(countryMap.getCapital("India"));
+  countryMap.saveCountryCapital("India", "Delhi");
 
-System.out.println(countryMap.getCountry("Tokyo"));
+  countryMap.saveCountryCapital("Japan", "Tokyo");
 
-System.out.println(countryMap.toArrayList());
-
-
-
-HashMap<String, String> M2 = countryMap.swapKyeValue();
-
-System.out.println(M2);
-
-}
+  countryMap.saveCountryCapital("USA", "Washington, D.C.");
 
 
 
+  System.out.println(countryMap.getCapital("India"));
+
+  System.out.println(countryMap.getCountry("Tokyo"));
+
+  System.out.println(countryMap.toArrayList());
 
 
+
+  HashMap<String, String> M2 = countryMap.swapKyeValue();
+
+  System.out.println(M2);
+
+  }
 }
