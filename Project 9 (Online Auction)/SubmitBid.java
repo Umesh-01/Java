302b212052d0869888e9dd199877fe387eb59a23
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-05-09 23:55:16 +0530

    Create SubmitBid.java

---
+++ Project 9 (Online Auction)/SubmitBid.java
@@ -0,0 +1,82 @@
+package com.auction.servlets;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/bid")
+public class SubmitBid extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Boolean isAutoInc = false;
+		HttpSession session = request.getSession();
+		int itemID = 0;
+		List lt = new LinkedList();
+		lt.add("Data Incomplete");
+		String itemid = request.getParameter("itemid");
+		if (itemid.length() == 0) {
+
+			lt.add("1");
+		} else {
+
+			itemID = Integer.parseInt(request.getParameter("itemid"));
+		}
+
+		String itemName = request.getParameter("itemname");
+		String userName = request.getParameter("username");
+		String userEmail = request.getParameter("useremail");
+		String bidAmt = request.getParameter("bidamount");
+		double bidAmount = 0;
+		if (!(bidAmt.length() == 0))
+			bidAmount = Double.parseDouble(request.getParameter("bidamount"));
+
+		String checkAutoInc = request.getParameter("isautoinc");
+		if (checkAutoInc == null)
+			isAutoInc = false;
+		else
+			isAutoInc = true;
+
+		if (itemName.length() == 0) {
+			lt.add("2");
+		}
+		if (userName.length() == 0) {
+			lt.add("3");
+		}
+		if (userEmail.length() == 0) {
+			lt.add("4");
+		}
+		if (bidAmt.length() == 0) {
+			lt.add("5");
+		}
+		if (!lt.isEmpty() && lt.size() > 1) {
+
+			session.setAttribute("errors", lt);
+
+			response.sendRedirect("index.jsp");
+
+		} else {
+			session.setAttribute("itemid", itemID);
+			session.setAttribute("itemname", itemName);
+			session.setAttribute("username", userName);
+			session.setAttribute("useremail", userEmail);
+			session.setAttribute("bidamount", bidAmount);
+			session.setAttribute("isautoinc", isAutoInc);
+			response.sendRedirect("showdetails.jsp");
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
