f81f37bc862a802be7597f477bc4101e927a5cca
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-05-11 22:14:09 +0530

    Update StringSeparator.java

--- Assignments/StringSeparator.java
+++ Assignments/StringSeparator.java
@@ -1,12 +1,6 @@
 package extra;
 
-
-
 public class StringSeparator {
-
-
-
-
 
 public static void main(String[] args) {
 
@@ -16,26 +10,16 @@ String str2 = "X";
 
 int separator = 3;
 
-
-
 StringBuffer sb = new StringBuffer();
-
-
 
 for (int i = 0 ; i < separator - 1; i++) 
 
 sb.append(str1).append(str2);
 
-
-
 sb.append(str1);
-
-
 
 System.out.println(sb);
 
+  }
+
 }
-
-
-
- }
