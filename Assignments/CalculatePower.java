c55738588234b66753dc18c28f08becc8ff565c0
Umesh Singh <83420185+Umesh-01@users.noreply.github.com>
2022-05-18 22:15:42 +0530

    Update CalculatePower.java

--- Assignments/CalculatePower.java
+++ Assignments/CalculatePower.java
@@ -1,6 +1,5 @@
 package extra;
 
-
 class Calculator {
 
 public static int powerInt(int num1, int num2) {
@@ -8,8 +7,6 @@ public static int powerInt(int num1, int num2) {
 return (int) Math.pow(num1, num2);
 
 }
-
-
 
 public static double powerDouble(double num1, int num2) {
 
